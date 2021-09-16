70f3d674830ee0304aa0d636b5c96f9b770b01b8
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2021-09-17 00:53:30 +0530

    Update apitrial.py

--- FileDump/apitrial.py
+++ FileDump/apitrial.py
@@ -94,4 +94,4 @@ def success(name):
     return "<xmp>" + str(requestResults(name)) + " </xmp> "
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=80, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
